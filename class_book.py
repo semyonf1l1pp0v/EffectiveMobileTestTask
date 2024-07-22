3e2f0fcd89b01d229c3980c972477e177082cd02
semyonf1l1pp0v <syomchik13@gmail.com>
2024-07-23 02:20:05 +0300

    Working on class Book

---
+++ class_book.py
@@ -0,0 +1,15 @@
+class Book:
+    __id: int = None
+    __title: str = None
+    __author: str = None
+    __year: int = None
+    __status: str = None
+
+    # def __init__(self, id: int, title: str, author: str, year: int, status: str = "В наличии")
+    # def set_id
+    # def set_title
+    # def set_author
+    # def set_year
+    # def set_status
+
+
