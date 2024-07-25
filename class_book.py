8145f00123073f5302e731920a9d8ce9bfdaec42
semyonf1l1pp0v <syomchik13@gmail.com>
2024-07-25 20:48:29 +0300

    Started working on documentation

--- class_book.py
+++ class_book.py
@@ -1,4 +1,5 @@
 class Book:
+    """Класс Книга, атрибуты - id, название, автор, год выпуска, статус (в наличии или выдана)"""
     __book_id: int = None
     __title: str = None
     __author: str = None
@@ -6,6 +7,7 @@ class Book:
     __status: str = None
 
     def __init__(self, book_id: int = 0, title: str = "", author: str = "", year: int = 0, status: str = "В наличии") -> None:
+        """Конструктор класса Книга"""
         self.set_id(book_id)
         self.set_title(title)
         self.set_author(author)
@@ -13,31 +15,41 @@ class Book:
         self.set_status(status)
 
     def set_id(self, book_id: int) -> None:
+        """Сеттер для атрибута id класса Книга"""
         self.__book_id = book_id
 
     def set_title(self, title: str) -> None:
+        """Сеттер для атрибута название класса Книга"""
         self.__title = title
 
     def set_author(self, author: str) -> None:
+        """Сеттер для атрибута автор класса Книга"""
         self.__author = author
 
     def set_year(self, year: int) -> None:
+        """Сеттер для атрибута год выпуска класса Книга"""
         self.__year = year
 
     def set_status(self, status: str) -> None:
+        """Сеттер для атрибута статус класса Книга"""
         self.__status = status
 
     def get_id(self) -> int:
+        """Геттер для атрибута id класса Книга"""
         return self.__book_id
 
     def get_title(self) -> str:
+        """Геттер для атрибута название класса Книга"""
         return self.__title
 
     def get_author(self) -> str:
+        """Геттер для атрибута автор класса Книга"""
         return self.__author
 
     def get_year(self) -> int:
+        """Геттер для атрибута год выпуска класса Книга"""
         return self.__year
 
     def get_status(self) -> str:
+        """Геттер для атрибута статус класса Книга"""
         return self.__status
